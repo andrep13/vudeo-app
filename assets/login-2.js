9b5f6bb3d3d9621644da4a78daf056d23355cb98
andrep13 <andrepermanaaz@gmail.com>
2021-09-26 20:19:35 +0700

    update

---
+++ assets/login-2.js
@@ -0,0 +1,106 @@
+// Your web app's Firebase configuration (ENTER YOUR FIREBASE CONFIGURATION DETAILS)
+var firebaseConfig = {
+apiKey: "AIzaSyDy7v7xQhooj-PRIPwofhf1NliLq22j4z0",
+  authDomain: "vudeo-b7cfc.firebaseapp.com",
+  projectId: "vudeo-b7cfc",
+  storageBucket: "vudeo-b7cfc.appspot.com",
+  messagingSenderId: "196924786214",
+  appId: "1:196924786214:web:b2565066b0489ec31e08e7",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var form = document.querySelector('#loginForm');
+var r_form = document.querySelector('#registerForm');
+var reset_form = document.querySelector('#resetForm');
+var message = document.querySelector('#messageDiv');
+var message_value = document.querySelector('.message');
+var sign_out = document.querySelector("#signOut");
+
+// check if user is logged in or not
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        if(window.location.pathname != '/dashboard.html'){
+            window.location = '../dashboard.html';
+        }
+    } else {
+        if(window.location.pathname === '/../dashboard.html'){
+            window.location = 'index.html';
+        }
+    }
+});
+
+// user login
+if(form){
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        let email = form.email.value;
+        let password = form.password.value;
+    
+        firebase.auth().signInWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            window.location = '../dashboard.html';
+        })
+        .catch((error) => {
+            message.style.display = 'block';
+            message_value.innerText = error.message;
+            setTimeout(function(){
+                message.style.display = 'none';
+            }, 3000);
+        });
+    })
+}
+
+// user register
+if(r_form){
+    r_form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        let email = r_form.email.value;
+        let password = r_form.password.value;
+    
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            window.location = '../dashboard.html';
+        })
+        .catch((error) => {
+            message.style.display = 'block';
+            message_value.innerText = error.message;
+            setTimeout(function(){
+                message.style.display = 'none';
+            }, 3000);
+        });
+    })
+}
+
+// password reset 
+if(reset_form){
+    reset_form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        let email = reset_form.email.value;
+    
+        firebase.auth().sendPasswordResetEmail(email)
+        .then((userCredential) => {
+            message.style.display = 'block';
+            message_value.innerText = 'Email has been send!';
+            
+        })
+        .catch((error) => {
+            message.style.display = 'block';
+            message_value.innerText = error.message;
+            setTimeout(function(){
+                message.style.display = 'none';
+            }, 3000);
+        });
+    })
+}
+
+// sign out  
+if(sign_out){
+    sign_out.addEventListener('click', function(e) {
+        firebase.auth().signOut().then(() => {
+            window.location = 'index.html';
+        }).catch((error) => {
+        // An error happened.
+        });
+    })
+}
